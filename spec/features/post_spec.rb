d01f6de716a1743a97fb37f690ebb6b5ef4fb920
supermancbs <clark.sacktor@gmail.com>
2016-07-04 11:47:39 -0400

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,9 +14,8 @@ describe 'new post' do
   it 'shows a new form that submits content and redirects to new page and prints out params' do
     visit new_post_path
 
-    fill_in 'title', with: "My post title"
-    fill_in 'description', with: "My post description"
-
+   fill_in 'title', with: "Post title:"
+fill_in 'description', with: "My post description"
     click_on "Submit Post"
 
     expect(page).to have_content("My post title")
